31fc6514af92bd6d82d6441a07e82e61fb0cc7be
jmarmstrong1207 <jm.armstrong1207@gmail.com>
2023-03-10 01:37:10 -0800

    Make currentUser context; Finish issue #39

--- src/components/data-table.tsx
+++ src/components/data-table.tsx
@@ -2,7 +2,6 @@ import useJaneHopkins from "@/api/useJaneHopkins";
 import Users from "@/api/Users";
 import { DataGrid, GridColDef, GridRowModel } from "@mui/x-data-grid"
 import React, { useEffect } from "react";
-import AddPatientForm from "./AddPatientForm";
 
 
 function removePII(columns: GridColDef[]) {
