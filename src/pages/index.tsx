f30bb7ab1e66c3b3f8d3a88d328bd4d498360631
JohnnyNguyen52 <johnnynguyen5200@gmail.com>
2023-03-09 18:24:39 -0800

    Data Table fills with patients from the database. Updates in real time.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,52 +1,329 @@
-import useJaneHopkins from '../hooks/useJaneHopkins';
+import React, { useState, useEffect, useReducer } from "react";
 import ResponsiveAppBar from '@/components/appbar';
-import { Typography } from '@mui/material';
+import { Typography,Button, Box, Stack, Modal, TextField, OutlinedInput,FormControl,InputAdornment, FormHelperText} from '@mui/material';
 import DataTable from '@/components/data-table';
-import { GridColDef, GridColumns, GridRowModel, GridRowsProp } from '@mui/x-data-grid';
-import React from 'react';
+
+
+import useJaneHopkins from '../hooks/useJaneHopkins';
 
 export default function Home() {
   const { entities } = useJaneHopkins();
+  const [patients, setPatients] = React.useState<any[]>([]);
+  const [openViewModal, setOpenViewModal] = useState(false);//set to false so that it is closed
 
-  //Allows me add patients to JaneHopkins system.
-  const addPatient = async () => {
+  
+
+  const handleCloseView = () => setOpenViewModal(false);
+
+//This stores the info that you inputted.
+  const [formInput, setFormInput] = useReducer(
+    (state: any, newState: any) => ({ ...state, ...newState }),
+    {
+      name: "",
+      dob: "",
+      height: "",
+      weight: "",
+      insuranceNumber: "",
+      address:"",
+
+    }
+  );
+
+  //This creates the "name: formInput[0].name" to be set into the formInput that is above this.
+  const handleInput = (evt: any) => {
+    const name = evt.target.name;
+    const newValue = evt.target.value;
+    setFormInput({ [name]: newValue });
+    
+  };
+//Takes the information inputted into the formInput and stores it into the Vendia Database
+  const handleAdd = async () => {
     const addPatientResponse = await entities.patient.add({
-      name: "Vanessa",
-      dob: "March 3, 2000",
-      insuranceNumber: "123456789",
-    });
-    console.log(addPatientResponse);
-  }
+      name:formInput.name,
+      dob:formInput.dob,
+      height:formInput.height,
+      weight:formInput.weight,
+      insuranceNumber:formInput.insuranceNumber,
+      address:formInput.address,
+      },
+      {
+        aclInput:{
+          acl:[
+            {
+              principal:{
+                nodes:["Bavaria", "FDA"]
+              },
+              operations: ["READ"], //Write gives the option to update
+              path:"weight",//What is allowed to go to Bavaria and FDA
+            },
+          ],
+        },
+      }
+    );
+    
+    setOpenViewModal(false); //Closes it
+  };
+  //This will reset the appearance of the form. Making blank once you enter it again.
+  //Probably a better way to do this.
+  const resetFormInput = async () =>{
+    console.log("reached here");
+    formInput.name = "";
+    formInput.dob = "";
+    formInput.height = "";
+    formInput.weight = "";
+    formInput.insuranceNumber = "";
+    formInput.address = "";
+  };
 
-  //////////////
-  // Example local data
-  const columns: GridColDef[] = [
-    { field: 'id', headerName: 'ID', width: 10 },
-    { field: 'name', headerName: 'Name', width: 250 },
-    { field: 'age', headerName: 'Age', width: 10 }
-  ]
-  const rows = [
-    { id: 1, name: 'James A', age: 1 },
-    { id: 2, name: 'James B', age: 2 },
-    { id: 3, name: 'James asdasdasdB', age: 3 },
-  ]
-  //////////////
+  let rows: any[]= [];
+
+  const columns= [
+    { field: 'id', headerName: 'ID', width: 150 },
+    { field: 'name', headerName: 'Name', width: 150 },
+    { field: 'dob', headerName: 'Date of Birth', width: 175 },
+    { field: 'height', headerName: 'Height', width: 100 },
+    { field: 'weight', headerName: 'Weight', width: 100 },
+    { field: 'address', headerName: 'Address', width: 150 },
+    { field: 'insuranceNumber', headerName: 'Insurance Number', width: 300 },
+    { field: 'patientPicture', headerName: 'Patient Picture', width: 125 },
+    { field: 'bloodPressure', headerName: 'Blood Pressure', width: 100 },
+    { field: 'temperature', headerName: 'Temperature', width: 100 },
+    { field: 'oxygenSaturation', headerName: 'Oxygen Saturation', width: 100 },
+    { field: 'uuid', headerName: 'UUID', width: 100 },
+    { field: 'currentMedications', headerName: 'Current Medications', width: 100 },
+    { field: 'familyHistory', headerName: 'Family History', width: 100 },
+    { field: 'currentlyInsured', headerName: 'Currently Insured', width: 100 },
+    { field: 'icdHealthCodes', headerName: 'ICD Health Codes', width: 100 },
+    { field: 'allergies', headerName: 'Allergies', width: 100 },
+  ];
+
+
+
+
+  //List patients
+  const listPatients = async () => {
+    let patientList = await entities.patient.list();
+    setPatients(patientList.items);
+    
+  };
+  //Pushes out the info to the data table.
+  for (let i = 0; i < patients.length; i++) {
+  rows.push({
+    id: patients[i]._id,
+    name: patients[i].name,
+    dob: patients[i].dob,
+    height: patients[i].height,
+    weight: patients[i].weight,
+    address: patients[i].address,
+    insuranceNumber: patients[i].insuranceNumber,
+    patientPicture: patients[i].patientPicture,
+    bloodPressure: patients[i].bloodPressure,
+    temperature: patients[i].temperature,
+    oxygenSaturation: patients[i].oxygenSaturation,
+    uuid: patients[i].uuid,
+    currentMedications: patients[i].currentMedications,
+    familyHistory: patients[i].familyHistory,
+    currentlyInsured: patients[i].currentlyInsured,
+    icdHealthCodes: patients[i].icdHealthCodes,
+    allergies: patients[i].allergies,
+     }); 
+    }
+
+  useEffect(() => {
+    listPatients();
+    });
+
+   
 
   return (
     <>
       <ResponsiveAppBar />
-      <br></br>
-      <button
-        onClick={() => {
-          addPatient();
-        }}
-      >Add Patient
-      </button>
-      <Typography variant='h3' align='center'>Example table</Typography>
+      <Button 
+          variant="contained"
+          onClick={() => {
+            resetFormInput();
+            setOpenViewModal(true);
+            
+          }}
+        >Add
+      </Button>
       <DataTable
-        columns={columns}
         rows={rows}
+        columns={columns}
+        
       />
+      <div>
+      <Modal
+        open={openViewModal}
+        onClose={handleCloseView}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        sx={{ zIndex: 1059 }}
+      >
+        <form onSubmit={handleAdd}>
+        <Box 
+          sx={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          width: 400,
+          backgroundColor: "background.paper",
+          border: "2px solid #000",
+          boxShadow: 24,
+          p: 2,
+          }}
+        >
+          <Box sx={{
+            display: "flex",
+            justifyContent: "left",
+            alignItems: "center",
+            m:2,      
+            }}
+          >
+            <TextField sx = {{m: 1,width: 200 }} helperText="Name" name="name" onChange={handleInput} defaultValue={formInput.name} />
+            <TextField type="date" sx = {{m: 1,width: 350 }} helperText="Date of Birth" name="dob" onChange={handleInput} defaultValue={formInput.dob} />
+          </Box>
+          <Box sx={{
+            display: "flex",
+            justifyContent: "left",
+            alignItems: "center",
+            m:2,      
+            }}
+          >
+            <TextField sx = {{m: 1,width: 200 }} helperText="Address" name="address" onChange={handleInput}defaultValue={formInput.address} />
+          </Box>
+          <Box sx={{
+            display: "flex",
+            justifyContent: "left",
+            alignItems: "center",
+            m:2,      
+            }}
+          >
+            <TextField sx = {{m: 1,width: 200 }} helperText="Insurance" name="insuranceNumber" onChange={handleInput} defaultValue={formInput.insuranceNumber}/>
+          </Box>
+          <Box sx={{
+            display: "flex",
+            justifyContent: "left",
+            alignItems: "center",
+            m:2,      
+            }}
+          >
+            <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+            <OutlinedInput
+              id="outlined-adornment-height"
+              aria-describedby="outlined-height-helper-text"
+              inputProps={{
+                'aria-label': 'height',
+              }}
+              name="height"
+              onChange={handleInput}
+              defaultValue={formInput.height}
+            />
+            <FormHelperText id="outlined-height-helper-text">Height</FormHelperText>
+            </FormControl>
+            <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+            <OutlinedInput
+              id="outlined-adornment-weight"
+              endAdornment={<InputAdornment position="end">lbs</InputAdornment>}
+              aria-describedby="outlined-weight-helper-text"
+              inputProps={{
+                'aria-label': 'weight',
+              }}
+              name="weight"
+              onChange={handleInput}
+              defaultValue={formInput.weight}
+              
+            />
+            <FormHelperText id="outlined-weight-helper-text">Weight</FormHelperText>
+            </FormControl>
+            
+            </Box>
+        <Box>
+          <Button 
+            variant="contained"
+              onClick={handleAdd}
+            >Add Patient
+          </Button>
+            <Button 
+            variant="outlined"
+            onClick={() => {
+              setOpenViewModal(false);
+            }}
+          >Cancel
+          </Button></Box>
+        </Box>
+        </form>
+      </Modal>
+      
+    </div>
     </>
   );
 }
+
+
+
+/*
+//Allows for the changing of buttons from one to another.
+{openViewModal === false ? (
+        <Stack
+          sx ={{ pt:4}}
+          direction = "row"
+          justifyContent = "left"
+          >
+        <Button 
+          variant="contained"
+          onClick={() => {
+            setOpenViewModal(true);
+          }}
+        >Add
+        </Button>
+
+        </Stack>
+        ):(
+        <Stack
+          sx ={{ pt:4}}
+          direction = "row"
+          justifyContent = "left"
+        >
+          <Button 
+          variant="outlined"
+          onClick={() => {
+            setOpenViewModal(false);
+          }}
+        >Cancel
+        </Button>
+        
+        </Stack> 
+        )
+      }
+
+<Box
+  component = "form"
+    sx={{
+      bgcolor: "white",
+      pt: 8,
+      pb: 6,
+      borderTop: "2px solid blue",
+    }}>
+  <Typography style = {{marginLeft: 60}}> {format}</Typography>
+    <Button 
+  variant="contained"
+    onClick={handleAdd}
+  >Add Patient
+    </Button>
+  </Box>
+<Box
+      sx={{
+        bgcolor: "white",
+        pt: 8,
+        pb: 6,
+        borderTop: "2px solid blue",
+      }}>
+        <Typography style = {{marginLeft: 60}}> {format}</Typography>
+       
+      </Box>
+      
+      Shows stuff from the button
+      */
+      
