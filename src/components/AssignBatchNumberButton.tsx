766d949853e422046bab1a3716aae8ac65b8e0c1
jmarmstrong1207 <jm.armstrong1207@gmail.com>
2023-03-11 05:34:52 -0800

    add BatchNumberAPI, batch # & placebo to fda table

--- src/components/AssignBatchNumberButton.tsx
+++ src/components/AssignBatchNumberButton.tsx
@@ -1,9 +1,9 @@
-import AssignBatchNumber from "@/api/assignBatchNumber";
+import { BatchNumberAPI } from "@/api/BatchNumberAPI";
 import { Button } from "@mui/material";
 
 export default function AssignBatchNumberButton()
 {
     return(
-        <Button variant='contained' onClick={() => {AssignBatchNumber()}}>Assign Batch Numbers</Button>
+        <Button variant='contained' onClick={() => {BatchNumberAPI.AssignBatchNumber()}}>Assign Batch Numbers</Button>
     )
 }
