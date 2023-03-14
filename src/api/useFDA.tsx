68dccfd95588d417244d16b1a766a4d69562b616
jmarmstrong1207 <jm.armstrong1207@gmail.com>
2023-03-14 06:51:43 -0700

    Fix patientEligibility code

--- src/api/useFDA.tsx
+++ src/api/useFDA.tsx
@@ -16,25 +16,6 @@ type Patient = {
 };
 
 
-// Define function to check patient eligibility
-const checkPatientEligibility = async (patient: Patient): Promise<boolean> => {
-  const { code: icd10Code } = patient.icd10;
-  const dob = new Date(patient.dob);
-
-  // Exclude ICD-10 Pregnancy codes
-  if (icd10Code.startsWith('O09') || icd10Code.startsWith('O10')) {
-    return false;
-  }
-
-  // Exclude DOB greater than 1/1/2005
-  if (dob > new Date('2005-01-01')) {
-    return false;
-  }
-
-  // Add more eligibility checks here as needed
-
-  return true;
-};
 
 const useFDA = (): { entities: any[]; checkPatientEligibility: (patient: Patient) => Promise<boolean> } => {
   return { entities, checkPatientEligibility };
