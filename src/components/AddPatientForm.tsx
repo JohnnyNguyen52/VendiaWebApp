52484b66f5b04caf2522c0acef662fd90975a1c3
Murtazaho <murtazahosseini12@gmail.com>
2023-03-20 21:58:58 -0700

    Update AddPatientForm.tsx

--- src/components/AddPatientForm.tsx
+++ src/components/AddPatientForm.tsx
@@ -1,36 +1,12 @@
-import schema from '../../.vendia/schema.json';
-import { useState } from 'react';
+import useJaneHopkins from "@/api/useJaneHopkins";
+import { Box, Button, FormControl, FormHelperText, InputAdornment, Modal, OutlinedInput, TextField } from "@mui/material";
+import { useReducer, useState } from "react";
 
-const AddPatientForm = () => {
-  const [formData, setFormData] = useState({
-    name: '',
-    dob: '',
-    insuranceNumber: '',
-    height: '',
-    weight: '',
-    bloodPressure: '',
-    temperature: '',
-    oxygenSaturation: '',
-    address: '',
-    familyHistory: '',
-    currentlyEmployed: '',
-    currentlyInsured: '',
-    currentMedications: [],
-    icdHealthCodes: [],
-    allergies: [],
-    visits: [],
-    patientPicture: null
-  });
+function AddPatientForm()
+{
+    const { entities } = useJaneHopkins();
+    const [openViewModal, setOpenViewModal] = useState(false);//set to false so that it is closed
 
-<<<<<<< Updated upstream
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = event.target;
-    setFormData({
-      ...formData,
-      [name]: value
-    });
-  };
-=======
     const handleCloseView = () => setOpenViewModal(false);
   //This creates the "name: formInput[0].name" to be set into the formInput that is above this.
   const handleInput = (evt: any) => {
@@ -55,52 +31,9 @@ const AddPatientForm = () => {
       currentlyInsured: false,
       icdHealthCodes: [], // as string array
       allergies: [], // as string array
->>>>>>> Stashed changes
 
-const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-  const { name, files } = event.target;
-  if (files && files.length > 0) {
-    setFormData({
-      ...formData,
-      [name]: files[0]
-    });
-  }
-};
-
-
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    // validate the form data against the schema
-    // send the data to the server
-  };
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Name:
-        <input type="text" name="name" value={formData.name} onChange={handleInputChange} />
-      </label>
-      <label>
-        Date of Birth:
-        <input type="text" name="dob" value={formData.dob} onChange={handleInputChange} />
-      </label>
-      <label>
-        Insurance Number:
-        <input type="text" name="insuranceNumber" value={formData.insuranceNumber} onChange={handleInputChange} />
-      </label>
-      <label>
-        Patient Picture:
-        <input type="file" name="patientPicture" onChange={handleFileChange} />
-      </label>
-      {/* Add more input fields for other properties */}
-      <button type="submit">Submit</button>
-    </form>
+    }
   );
-<<<<<<< Updated upstream
-};
-
-export default AddPatientForm;
-=======
 //Takes the information inputted into the formInput and stores it into the Vendia Database
   const handleAdd = async () => {
     try {
@@ -276,4 +209,3 @@ export default AddPatientForm;
 }
 
 export default AddPatientForm;
->>>>>>> Stashed changes
