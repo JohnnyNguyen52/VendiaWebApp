00da4254df236fa4119a4829aaf6b9421d1d95e0
Murtazaho <murtazahosseini12@gmail.com>
2023-03-26 15:19:21 -0700

    Update data-table.tsx

--- src/components/data-table.tsx
+++ src/components/data-table.tsx
@@ -1,9 +1,6 @@
 import { BatchNumberAPI } from "@/api/BatchNumberAPI";
 import useJaneHopkins from "@/api/useJaneHopkins";
-<<<<<<< Updated upstream
-=======
-import useRefreshKey from "../api/useRefreshKey";
->>>>>>> Stashed changes
+
 import Users from "@/api/Users";
 import { DataGrid, GridColDef, GridRowModel } from "@mui/x-data-grid"
 import React, { useEffect } from "react";
