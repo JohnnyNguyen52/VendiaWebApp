1a6dcfd9232453d12c9ef919690da01f1b02a064
minhhieutle <66087649+minhhieutle@users.noreply.github.com>
2023-03-27 15:44:59 -0700

    Update appbar (#56)
    * updated the appbar
    
    * added 2 buttons to appbar and fixed overlap issue
    
    * add VendiaAPI, make studybutton use REST backend
    
    * Added and styled batch number and study button
    
    * Remove buttons on home page
    
    ---------
    
    Co-authored-by: jmarmstrong1207 <jm.armstrong1207@gmail.com>

--- src/components/AssignBatchNumberButton.tsx
+++ src/components/AssignBatchNumberButton.tsx
@@ -1,9 +1,18 @@
-import { BatchNumberAPI } from "@/api/BatchNumberAPI";
-import { Button } from "@mui/material";
+import { VendiaWebAppAPI } from "@/api/VendiaWebAppAPI";
+import { Button, ListItemButton, ListItemIcon } from "@mui/material";
+import AssignmentTurnedInIcon from '@mui/icons-material/AssignmentTurnedIn';
+import AssignmentTurnedIn from "@mui/icons-material/AssignmentTurnedIn";
 
 export default function AssignBatchNumberButton()
 {
+    const buttonText = 'Assign Batch Number';
     return(
-        <Button variant='contained' onClick={() => {BatchNumberAPI.AssignBatchNumber()}}>Assign Batch Numbers</Button>
-    )
+        //<Button variant='text' onClick={() => {VendiaWebAppAPI.AssignBatchNumber()}}>Assign Batch Numbers</Button>
+        <ListItemButton onClick={() => {VendiaWebAppAPI.AssignBatchNumber()}}>
+        <ListItemIcon sx={{minWidth: 0, mr: 3, justifyContent: 'center'}}>
+            <AssignmentTurnedInIcon/>
+        </ListItemIcon>
+        {buttonText}
+        </ListItemButton>
+     )
 }
--- src/components/appbar.tsx
+++ src/components/appbar.tsx
@@ -1,15 +1,5 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
 import { styled, useTheme, Theme, CSSObject } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import Divider from '@mui/material/Divider';
@@ -18,11 +8,9 @@ import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import List from '@mui/material/List';
-import MailIcon from '@mui/icons-material/Mail';
 import HomeIcon from '@mui/icons-material/Home';
 import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
-import AccountBoxIcon from '@mui/icons-material/AccountBox';
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import LogoutIcon from '@mui/icons-material/Logout';
 import HelpCenterIcon from '@mui/icons-material/HelpCenter';
@@ -32,10 +20,20 @@ import IconButton from '@mui/material/IconButton';
 import MenuOpenIcon from '@mui/icons-material/MenuOpen';
 import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
 import PersonIcon from '@mui/icons-material/Person';
+import AssignmentTurnedInIcon from '@mui/icons-material/AssignmentTurnedIn';
+import BiotechIcon from '@mui/icons-material/Biotech';
+import AssignBatchNumberButton from '@/components/AssignBatchNumberButton';
+import StartStudyButton from '@/components/StartStudyButton';
+import { start } from 'repl';
+
 
 const pages = ['Home', 'Products', 'Pricing', 'Help'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+const functionButtons = [StartStudyButton, AssignBatchNumberButton];
 const drawerWidth = 250;
+
+
+
 
 const openedMixin = (theme: Theme): CSSObject => ({
   width: drawerWidth,
@@ -108,24 +106,6 @@ const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' 
 
 
 export default function ResponsiveAppBar() {
-    // const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
-    // const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
-
-    // const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-    //     setAnchorElNav(event.currentTarget);
-    // };
-    // const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-    //     setAnchorElUser(event.currentTarget);
-    // };
-
-    // const handleCloseNavMenu = () => {
-    //     setAnchorElNav(null);
-    // };
-
-    // const handleCloseUserMenu = () => {
-    //     setAnchorElUser(null);
-    // };
-
 
     const theme = useTheme();
     const [open, setOpen] = React.useState(false);
@@ -216,135 +196,63 @@ export default function ResponsiveAppBar() {
           ))}
         </List>
         <Divider />
+        {/* {functionPages.map((text, index) => (
+            <ListItem key={text} disablePadding sx={{ display: 'block' }}>
+              <ListItemButton
+              sx={{
+                minHeight: 48,
+                justifyContent: open ? 'initial' : 'center',
+                px: 2.5,}}>
+                <ListItemIcon
+                sx={{
+                  minWidth: 0,
+                  mr: open ? 3 : 'auto',
+                  justifyContent: 'center',
+                }}>
+                  {index === 0 ? <BiotechIcon /> 
+                  : index === 1 ? <AssignmentTurnedInIcon />  : null} 
+                </ListItemIcon>
+                <ListItemText primary={text}  sx={{ opacity: open ? 1 : 0 }}/>
+              </ListItemButton>
+            </ListItem>
+          ))} */}
+          
+          <List>
+            <ListItem disablePadding sx={{ display: 'block' }}>
+              <StartStudyButton/> 
+              <AssignBatchNumberButton/>
+                <ListItemText sx={{ opacity: open ? 1 : 0 }}/>
+            </ListItem>
+        </List>
+
+
+
+        {/* <List>
+          {functionButtons.map((button, index) => (
+            <ListItem key={button.name} disablePadding sx={{ display: 'block' }}>
+              <ListItemButton
+              sx={{
+                minHeight: 48,
+                justifyContent: open ? 'initial' : 'center',
+                px: 2.5,}}>
+                <ListItemIcon
+                sx={{
+                  minWidth: 0,
+                  mr: open ? 3 : 'auto',
+                  justifyContent: 'center',
+                }}>
+                  {index === 0 ? <BiotechIcon /> 
+                  : index === 1 ? <AssignmentTurnedInIcon /> : null} 
+                </ListItemIcon>
+                <ListItemText primary={button.name}  sx={{ opacity: open ? 1 : 0 }}/>
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List> */}
+        
       </Drawer>
       
-        <Toolbar />
       </Box>
 
     );
-
-
-    // return (
-    //     <AppBar position="static">
-    //         <Container maxWidth="xl">
-    //             <Toolbar disableGutters>
-    //                 <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-    //                 <Typography
-    //                     variant="h6"
-    //                     noWrap
-    //                     component="a"
-    //                     href="/"
-    //                     sx={{
-    //                         mr: 2,
-    //                         display: { xs: 'none', md: 'flex' },
-    //                         fontFamily: 'monospace',
-    //                         fontWeight: 700,
-    //                         letterSpacing: '.3rem',
-    //                         color: 'inherit',
-    //                         textDecoration: 'none',
-    //                     }}
-    //                 >
-    //                     LOGO
-    //                 </Typography>
-
-    //                 <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-    //                     <IconButton
-    //                         size="large"
-    //                         aria-label="account of current user"
-    //                         aria-controls="menu-appbar"
-    //                         aria-haspopup="true"
-    //                         onClick={handleOpenNavMenu}
-    //                         color="inherit"
-    //                     >
-    //                         <MenuIcon />
-    //                     </IconButton>
-    //                     <Menu
-    //                         id="menu-appbar"
-    //                         anchorEl={anchorElNav}
-    //                         anchorOrigin={{
-    //                             vertical: 'bottom',
-    //                             horizontal: 'left',
-    //                         }}
-    //                         keepMounted
-    //                         transformOrigin={{
-    //                             vertical: 'top',
-    //                             horizontal: 'left',
-    //                         }}
-    //                         open={Boolean(anchorElNav)}
-    //                         onClose={handleCloseNavMenu}
-    //                         sx={{
-    //                             display: { xs: 'block', md: 'none' },
-    //                         }}
-    //                     >
-    //                         {pages.map((page) => (
-    //                             <MenuItem key={page} onClick={handleCloseNavMenu}>
-    //                                 <Typography textAlign="center">{page}</Typography>
-    //                             </MenuItem>
-    //                         ))}
-    //                     </Menu>
-    //                 </Box>
-    //                 <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
-    //                 <Typography
-    //                     variant="h5"
-    //                     noWrap
-    //                     component="a"
-    //                     href=""
-    //                     sx={{
-    //                         mr: 2,
-    //                         display: { xs: 'flex', md: 'none' },
-    //                         flexGrow: 1,
-    //                         fontFamily: 'monospace',
-    //                         fontWeight: 700,
-    //                         letterSpacing: '.3rem',
-    //                         color: 'inherit',
-    //                         textDecoration: 'none',
-    //                     }}
-    //                 >
-    //                     LOGO
-    //                 </Typography>
-    //                 <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-    //                     {pages.map((page) => (
-    //                         <Button
-    //                             key={page}
-    //                             onClick={handleCloseNavMenu}
-    //                             sx={{ my: 2, color: 'white', display: 'block' }}
-    //                         >
-    //                             {page}
-    //                         </Button>
-    //                     ))}
-    //                 </Box>
-
-    //                 <Box sx={{ flexGrow: 0 }}>
-    //                     <Tooltip title="Open settings">
-    //                         <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-    //                             <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-    //                         </IconButton>
-    //                     </Tooltip>
-    //                     <Menu
-    //                         sx={{ mt: '45px' }}
-    //                         id="menu-appbar"
-    //                         anchorEl={anchorElUser}
-    //                         anchorOrigin={{
-    //                             vertical: 'top',
-    //                             horizontal: 'right',
-    //                         }}
-    //                         keepMounted
-    //                         transformOrigin={{
-    //                             vertical: 'top',
-    //                             horizontal: 'right',
-    //                         }}
-    //                         open={Boolean(anchorElUser)}
-    //                         onClose={handleCloseUserMenu}
-    //                     >
-    //                         {settings.map((setting) => (
-    //                             <MenuItem key={setting} onClick={handleCloseUserMenu}>
-    //                                 <Typography textAlign="center">{setting}</Typography>
-    //                             </MenuItem>
-    //                         ))}
-    //                     </Menu>
-    //                 </Box>
-    //             </Toolbar>
-    //         </Container>
-    //     </AppBar>
-    // );
 }
